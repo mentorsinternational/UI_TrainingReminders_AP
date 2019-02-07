4c2e3ff08f4665f708562673594b50bd6e509393
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-02-07 15:55:40 -0800

    Project completed, all polished and Team Page Completed

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,12 +1,15 @@
+
+
 let successImg = document.querySelectorAll(".success-img"); 
-successImg.forEach(img => {
-    img.addEventListener("mouseover", (e) => {
-        TweenMax.to(e.target,.4, {transform:'scale(1.3)'});
+    successImg.forEach(img => {
+        img.addEventListener("mouseover", (e) => {
+            TweenMax.to(e.target,.4, {transform:'scale(1.07)'});
+        });
+                img.addEventListener("mouseout",(e) => {
+                    TweenMax.to(e.target,.4, {transform:'scale(1)' });
+        })
     });
-    img.addEventListener("mouseout",(e) => {
-        TweenMax.to(e.target,.4, {transform:'scale(1)' });
-    })
-});
 
 let keylog = ""
-window.addEventListener('keydown', (e) => keylog += e.key);
+    window.addEventListener('keydown', (e) => keylog += e.key);
+        console.log(keylog);
